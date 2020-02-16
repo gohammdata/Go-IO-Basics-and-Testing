cbba99da93b89dd126766fcf73c8a93ed5c57428
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-16 08:42:43 -0500

    Update main.go
    Added the towerofHanoi funciton and the main function to run and test the function.

--- Tower of Hanoi/main.go
+++ Tower of Hanoi/main.go
@@ -1,2 +1,23 @@
 //Tower of Hanoi Function
-//HammData
+
+package main
+
+import "fmt"
+
+
+func towerofHanoi(num int, src byte, dst byte, temp byte) {
+    if num < 1 {
+        return
+    }
+    
+    towerofHanoi(num-1, src, temp, dst)
+    fmt.Printf("Move %d dis, from peg %c to peg %c \n", num, src, dst)
+    towerofHanoi(num-1, temp, dst, src)
+}
+
+//Main function to run and test
+func main() {
+    num := 3
+    fmt.Println("The sequence of moves used for the Tower of Hanoi are listed  below:")
+    towerofHanoi(num, 'A', 'C', 'B')
+}
