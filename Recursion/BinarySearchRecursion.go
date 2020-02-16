45f9b8261778d65b8cf963c2abadd48b9b7a274e
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-16 08:56:27 -0500

    Update BSR
    functions and strings to test true and false

--- Recursion/BinarySearchRecursion.go
+++ Recursion/BinarySearchRecursion.go
@@ -1,1 +1,30 @@
 //Binary Search Recursion
+package main
+
+import "fmt"
+
+func BinarySearchRecursive(data []int, value int) bool {
+    size := len(data)
+    return BinarySearchRecursiveUtil(data, 0, size - 1, value)
+}
+
+func BinarySearchRecursiveUtil(data []int, low int, high int, value int) bool {
+    if low > high {
+        return false
+    }
+    mid := (low + high)/2
+    if data[mid] == value {
+        return true
+    } else if data [mid] < value {
+        return BinarySearchRecursiveUtil(data, mid+1, high, value)
+    } else {
+        return BinarySearchRecursiveUtil(data, low, mid-1, value)
+    }
+}
+
+func main() {
+    //sample array for search
+    arr := []int{1, 2, 3, 4, 5, 6,7, 9,}
+    fmt. Println("BinarySearchRecursive:", BinarySearchRecursive(arr, 7)) //true
+    fmt. Println("BinarySearchRecursive:", BinarySearchRecursive(arr, 8)) //false
+}
